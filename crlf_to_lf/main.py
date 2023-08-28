fa2db8aafc35c8be8122b46a6f07935b0136373d
Frexom <imfrexom@gmail.com>
2023-08-28 15:14:00 +0200

    Added reverse operation (lf to crlf)

--- crlf_to_lf/main.py
+++ crlf_to_lf/main.py
@@ -8,12 +8,17 @@ UNIX_LINE_ENDING = b'\n'
 
 
 def _fix_file(
-        filename: str
+        filename: str,
+        keep_crlf: bool
 ) -> bool:
     with open(filename, mode='rb') as file_processed:
         content = file_processed.read()
 
-    fixed_content = content.replace(WINDOWS_LINE_ENDING, UNIX_LINE_ENDING)
+    if not keep_crlf:
+        fixed_content = content.replace(WINDOWS_LINE_ENDING, UNIX_LINE_ENDING)
+    else:
+        fixed_content = content.replace(UNIX_LINE_ENDING, WINDOWS_LINE_ENDING)
+
     with open(filename, mode='wb') as file_processed:
         file_processed.write(fixed_content)
 
@@ -23,12 +28,13 @@ def _fix_file(
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
 
+    parser.add_argument('--keep-crlf', action='store_true')
     parser.add_argument('filenames', nargs='*', help='Filenames to fix')
     args = parser.parse_args(argv)
 
     return_code = 0
     for filename in args.filenames:
-        if _fix_file(filename):
+        if _fix_file(filename, args.keep_crlf):
             print(f"Fixing {filename}'s EOL...")
             return_code = 1
     return return_code
