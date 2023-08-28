ee46fed0b396fa83800189bdc321d76844178143
Frexom <imfrexom@gmail.com>
2023-08-28 15:15:03 +0200

    Command now runs from the repository root

--- pytest_check/main.py
+++ pytest_check/main.py
@@ -3,22 +3,23 @@ import re
 
 
 def main():
-    result = subprocess.run(["pytest", "-rf"], stdout=subprocess.PIPE)
+    root = subprocess.run(["git", "rev-parse", "--show-toplevel"], stdout = subprocess.PIPE)
+    root = root.stdout.decode("utf-8")[:-1]
+
+    result = subprocess.run(["pytest", "-rf", root], stdout=subprocess.PIPE)
     lines = result.stdout.decode('utf-8').split("\n")[:-1]
 
 
-    if "failed" in lines[-1]:
+    if "failed" in lines[-1] or "error" in lines[-1]:
         error_output(lines)
         exit(1)
-
-
 
 
 def error_output(lines: list[str]):
     print('\033[91m')
     error_report = False
     for line in lines:
-        if re.match("^==* FAILURES ==*", line):
+        if re.match("^==* FAILURES ==*", line) or re.match("^==* ERRORS ==*", line):
             error_report = True
 
         if error_report:
